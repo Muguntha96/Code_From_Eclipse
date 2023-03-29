6b35a34f6e00e0d2ef36f0d07fd8dfa75f6cf556
muguntha.r <muguntha.r@kgisl.com>
2023-03-29 11:21:36 +0530

    TestNG framework

---
+++ test555/src/test555/test.java
@@ -0,0 +1,13 @@
+package test555;
+
+import org.testng.annotations.Test;
+
+public class test {
+	
+	@Test
+	public void test1()
+	{
+		
+		System.out.println("Test");
+	}
+}
---
+++ test555/test/testNG/NG.java
@@ -0,0 +1,13 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class NG {
+
+	@Test
+	public void test1()
+	{
+		
+		System.out.println("Test");}
+	
+}
